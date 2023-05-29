5f8876c95698b35b72e7051972c632263dd502a6
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-05-29 17:24:46 +0300

    + shadcn/ui

--- src/components/navbar/logo/logo.tsx
+++ src/components/navbar/logo/logo.tsx
@@ -2,19 +2,21 @@ import { FC } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { cn } from '@/helpers/helpers';
+
 type Props = {
   className?: string;
 }
 
-const Logo: FC<Props> = ({ className="" }) => {
+const Logo: FC<Props> = ({ className }) => {
   return (
-    <Link className={className} href="/">
+    <Link className={cn(className)} href="/">
       <Image
         className="hidden max-w-full h-auto md:block"
         src="/images/logo.png"
-        width="100"
+        width="102"
         height="32"
-        alt="Logo"
+        alt="Airbnb logo"
       />
     </Link>
   );
