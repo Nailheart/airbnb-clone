14f280b0d473b7f486690068d8bf044780d3c4c7
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-06-12 12:03:59 +0300

    add cn to category-input component

--- src/components/category-input/category-input.tsx
+++ src/components/category-input/category-input.tsx
@@ -1,30 +1,32 @@
 import { FC } from 'react';
 import { IconType } from 'react-icons';
 
+import { cn } from '@/helpers/helpers';
+
 type Props = {
   label: string;
-  icon: IconType,
+  icon: IconType;
   selected?: boolean;
+  className?: string;
   onClick: (value: string) => void;
 }
 
-const CategoryInput: FC<Props> = ({ label, icon: Icon, selected, onClick}) => {
+const CategoryInput: FC<Props> = ({
+  label,
+  icon: Icon,
+  selected,
+  className,
+  onClick
+}) => {
   const handleClick = () => onClick(label);
 
   return (
     <div
-      className={`
-        flex
-        flex-col
-        gap-3
-        p-4
-        border-2
-        rounded-xl
-        cursor-pointer
-        hover:border-black
-        transition
-        ${selected ? 'border-black' : 'border-neutral-200'}
-      `}
+      className={cn(
+        'flex flex-col gap-3 p-4 border-2 border-neutral-200 rounded-xl cursor-pointer hover:border-black transition',
+        selected && 'border-black',
+        className,
+      )}
       onClick={handleClick}
     >
       <Icon size={30} />
