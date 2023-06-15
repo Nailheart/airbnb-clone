0e03405feaf23779c6a9d4241bb9219141884c5e
Yaroslav Lebedenko <nailheart4@gmail.com>
2023-06-15 15:23:15 +0300

    refactor input component

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,6 +4,8 @@ import { FC } from 'react';
 import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 import { BiDollar } from 'react-icons/bi';
 
+import { cn } from '@/helpers/helpers';
+
 type Props = {
   id: string;
   label: string;
@@ -11,8 +13,8 @@ type Props = {
   disabled?: boolean;
   formatPrice?: boolean;
   required?: boolean;
-  errors: FieldErrors
-  register: UseFormRegister<FieldValues>,
+  errors: FieldErrors;
+  register: UseFormRegister<FieldValues>;
 }
 
 const Input: FC<Props> = ({
@@ -32,47 +34,23 @@ const Input: FC<Props> = ({
       )}
       <input
         id={id}
-        className={`
-          peer
-          w-full
-          p-4
-          pt-6 
-          font-light 
-          bg-white 
-          border-2
-          rounded-md
-          outline-none
-          transition
-          disabled:opacity-70
-          disabled:cursor-not-allowed
-          ${formatPrice ? 'pl-9' : 'pl-4'}
-          ${errors[id]
-            ? 'border-rose-500 focus:border-rose-500'
-            : 'border-neutral-300 focus:border-black'
-          }
-        `}
+        className={cn(
+          'peer w-full p-4 bg-white border-2 border-neutral-300 rounded-md outline-none transition focus:border-black disabled:opacity-70 disabled:cursor-not-allowed',
+          formatPrice && 'pl-9',
+          errors[id] && 'border-rose-500 focus:border-rose-500',
+        )}
         type={type}
         disabled={disabled}
         placeholder=" "
         {...register(id, { required })}
       />
       <label 
-        className={`
-          absolute
-          text-md
-          duration-150
-          transform
-          -translate-y-3
-          top-5
-          z-10
-          origin-[0]
-          peer-placeholder-shown:scale-100
-          peer-placeholder-shown:translate-y-0
-          peer-focus:scale-75
-          peer-focus:-translate-y-4
-          ${formatPrice ? 'left-9' : 'left-4'}
-          ${errors[id] ? 'text-rose-500' : 'text-zinc-400'}
-        `}
+        className={cn(
+          'absolute left-4 text-zinc-400 text-md px-2 -ml-2 bg-white rounded-sm duration-150 origin-[0] -translate-y-[50%] z-10 peer-focus:top-0 peer-focus:text-black peer-placeholder-shown:top-1/2',
+          formatPrice && 'left-9',
+          errors[id] && '!text-rose-500',
+        )}
+        htmlFor={id}
       >
         {label}
       </label>
